f05f2f9628c9a38ce265d8200eb19b9474fb6e65
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-20 20:32:34 +0500

    Added: title

--- Customer_Registration/lib/Customer_Registration/Controller/Root.pm
+++ Customer_Registration/lib/Customer_Registration/Controller/Root.pm
@@ -39,6 +39,7 @@ sub index :Path :Args(0) {
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Customer Registeration</title>
 </head>
 <body>
     <div class='block signupBlock'>
